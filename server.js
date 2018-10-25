a2a3464d87f4d2954c2614c60d1daae1dfef85c5
J Gabriel Gouveia <joao.gabriel.gouveia2@gmail.com>
2018-10-25 00:15:24 -0300

    code adjustment

--- server.js
+++ server.js
@@ -1,29 +1,56 @@
 const express = require('express');
 const path = require('path');
 
+const PORT = process.env.PORT || 8080;
+const IP = process.env.IP || 'localhost';
+
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'public'));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-
-app.use('/', (req, res) => {
-    res.render('index.html');
-});
+var socket;
 
 let messages = [];
 
-io.on('connection', socket => {
+initialize();
 
+function initialize() {
+    configure();
+    estabilishSocketConnection();
+    startHttpServer();
+}
+
+function configure() {
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.set('views', path.join(__dirname, 'public'));
+    app.engine('html', require('ejs').renderFile);
+    app.set('view engine', 'html');
+    
+    app.use('/', (req, res) => {
+        res.render('index.html');
+    });
+}
+
+function estabilishSocketConnection() {
+    io.on('connection', ioSocket => {
+        socket = ioSocket;
+        loadPreviousMessages();
+        configureSendMessageListener();
+    })
+}
+
+function loadPreviousMessages() {
     socket.emit('previousMessage', messages);
+}
 
+function configureSendMessageListener() {
     socket.on('sendMessage', data => {
         messages.push(data);
         socket.broadcast.emit('recivedMessage', data);
     });
-})
+}
 
-server.listen(process.env.PORT, process.env.IP);
+function startHttpServer() {
+    server.listen(PORT, IP);
+    console.log('Server started on http://' + IP + ':' + PORT);
+}
